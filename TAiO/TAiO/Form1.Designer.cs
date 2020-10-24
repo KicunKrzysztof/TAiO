64f304cd13876ea89f5713ff73be3a6a83e14994
KicunKrzysztof <kicunkrzysztof@gmail.com>
2020-10-24 17:31:14 +0200

    Added base UI

---
+++ TAiO/TAiO/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace TAiO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tetrisMatrix1 = new TAiO.TetrisMatrix();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tetrisMatrix1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanel1.Controls.Add(this.tetrisMatrix1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1582, 953);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tetrisMatrix1
+            // 
+            this.tetrisMatrix1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tetrisMatrix1.Location = new System.Drawing.Point(3, 3);
+            this.tetrisMatrix1.Name = "tetrisMatrix1";
+            this.tetrisMatrix1.Size = new System.Drawing.Size(1376, 947);
+            this.tetrisMatrix1.TabIndex = 0;
+            this.tetrisMatrix1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1385, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1582, 953);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tetrisMatrix1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private TetrisMatrix tetrisMatrix1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TAiO/TAiO/TetrisMatrix.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TAiO
+{
+    public partial class TetrisMatrix : PictureBox
+    {
+        private int[,] _matrix;
+        private List<Color> _colors;
+        public TetrisMatrix()
+        {
+            InitializeComponent();
+            InitMatrix();
+        }
+
+        public void PutMatrix(int [,] matrix)
+        {
+            _matrix = matrix;
+            this.Invalidate();
+            Console.WriteLine("kutas");
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+
+            //Bitmap btm = new Bitmap(this.Size.Width, this.Size.Height);
+
+            SolidBrush myBrush = new SolidBrush(Color.White);
+            //SolidBrush myBrush = new SolidBrush(Color.FromArgb(0,0,0));
+            pe.Graphics.FillRectangle(myBrush, new Rectangle(0, 0, Size.Width, Size.Height));
+            myBrush.Dispose();
+
+            int delta = Size.Height / _matrix.GetLength(0);
+            int y = 0, x = 0, a = delta*_matrix.GetLength(0);
+
+            GenerateColors();
+            List<SolidBrush> brushes = new List<SolidBrush>();
+            for (int i = 0; i < _colors.Count; i++)
+                brushes.Add(new SolidBrush(_colors[i]));
+            for (int i = 0; i < _matrix.GetLength(0); i++)
+                for (int j = 0; j < _matrix.GetLength(0); j++)
+                {
+                    pe.Graphics.FillRectangle(brushes[_matrix[i, j]], j * delta, i * delta, delta, delta);
+                }
+            for (int i = 0; i < _colors.Count; i++)
+                brushes[i].Dispose();
+            Pen pen = new Pen(Color.Black);
+            for(int i = 0; i <= _matrix.GetLength(0); i++)
+            {
+                pe.Graphics.DrawLine(pen, 0, y, a, y);
+                y += delta;
+                pe.Graphics.DrawLine(pen, x, 0, x, a);
+                x += delta;
+            }
+            pen.Dispose();
+
+            //Pen myPen = new Pen(Color.Aqua);
+            //pe.Graphics.DrawRectangle(myPen, new Rectangle(this.Location,
+            //   this.Size));
+            Console.WriteLine("dupa");
+
+        }
+        private void InitMatrix()
+        {
+            _matrix =  new int[30, 30];
+            for(int i = 0; i < _matrix.GetLength(0); i++)
+                for(int j = 0; j < _matrix.GetLength(0); j++)
+                {
+                    _matrix[i, j] = i;
+                }
+        }
+
+        private void GenerateColors()
+        {
+            Random rand = new Random();
+            _colors = new List<Color>();
+            HashSet<int> intSet = new HashSet<int>();
+            for (int i = 0; i < _matrix.GetLength(0); i++)
+                for (int j = 0; j < _matrix.GetLength(0); j++)
+                {
+                    if (!intSet.Contains(_matrix[i, j]))
+                    {
+                        intSet.Add(_matrix[i, j]);
+                    }
+                }
+            int delta = 255 / (intSet.Count + 1);//"+1" to avoid generating white color
+            int r = 255 - delta; //to avoid generating white color
+            _colors.Add(Color.White);
+            for (int i = 1; i<intSet.Count; i++)
+            {
+                _colors.Add(Color.FromArgb(r, rand.Next(255), rand.Next(255)));
+                r -= delta;
+            }
+        }
+    }
+}
